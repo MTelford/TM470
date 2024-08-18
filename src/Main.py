620bb909c181167915ebc822a5ad852842f3b164
MTelford <operator_141@outlook.com>
2024-08-18 20:06:52 +0100

    moved card deck out of main

--- src/Main.py
+++ src/Main.py
@@ -37,22 +37,10 @@ DISPLAYSURF = pygame.display.set_mode((1280, 720))
 DISPLAYSURF.fill(GREEN)
 pygame.display.set_caption("Jack Change It")
 
-# set up random game deck and intialise card management component
-# with card deck as deque for fast prepend/append
-
-card_deck = ["2C", "2S", "2D", "2H", "3C", "3S", "3D", "3H",
-             "4C", "4S", "4D", "4H", "5C", "5S", "5D", "5H",
-             "6C", "6S", "6D", "6H", "7C", "7S", "7D", "7H",
-             "8C", "8S", "8D", "8H", "9C", "9S", "9D", "9H",
-             "10C", "10S", "10D", "10H", "JC", "JS", "JD", "JH",
-             "QC", "QS", "QD", "QH", "KC", "KS", "KD", "KH",
-             "AC", "AS", "AD", "AH"]
-
-random.shuffle(card_deck)
 # pass in DISPLAYSURF to update screen within card management class
-card_management = CardManager(deque(card_deck), DISPLAYSURF)
-
-card_management.give_player_new_cards()
+# card_management = CardManager(deque(card_deck), DISPLAYSURF)
+#
+# card_management.give_player_new_cards()
 
 # Game loop
 
