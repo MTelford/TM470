78ad5b5f851dd2cb6269c6dbc019e5d68f1aee9f
MTelford <operator_141@outlook.com>
2024-08-19 22:16:14 +0100

    adjusted test case naming convention to get picked up, added resources

---
+++ tests/test_Card_methods.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestCardMethods(unittest.TestCase):
+
+
+
+    def test_get_new_card(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
