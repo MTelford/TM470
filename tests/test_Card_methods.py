0f891d26dbd3c2726826bd77d8f7c748b5f48317
MTelford <operator_141@outlook.com>
2024-08-19 22:22:14 +0100

    added another test case to test build pipeline on PRs

--- tests/test_Card_methods.py
+++ tests/test_Card_methods.py
@@ -11,6 +11,9 @@ class TestCardMethods(unittest.TestCase):
     def test_get_new_card2(self):
             self.assertEqual(True, True)  # add assertion here
 
+    def test_get_new_card3(self):
+        self.assertEqual(True, True)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
