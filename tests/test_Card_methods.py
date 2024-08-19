09112e11f67f95149c655207c93c9f5179ed180d
MTelford <operator_141@outlook.com>
2024-08-19 22:19:39 +0100

    added dummy test case to test build pipeline

--- tests/test_Card_methods.py
+++ tests/test_Card_methods.py
@@ -8,6 +8,9 @@ class TestCardMethods(unittest.TestCase):
     def test_get_new_card(self):
         self.assertEqual(True, True)  # add assertion here
 
+    def test_get_new_card2(self):
+            self.assertEqual(True, True)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
