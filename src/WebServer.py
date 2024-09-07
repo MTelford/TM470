b989e7f8ed2921fa4588482cb5fa2c6610264ad0
MTelford <operator_141@outlook.com>
2024-09-07 21:03:17 +0100

    added landing page index.html for web server functionality

--- src/WebServer.py
+++ src/WebServer.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Serve the image located in the 'static' folder
-    return send_from_directory('resources/cards/', '2H.png')
+    return send_from_directory('resources/', 'index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
