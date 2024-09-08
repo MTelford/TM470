0f05f94afef7effb61fbf31d5ff2f19a07177fd5
MTelford <operator_141@outlook.com>
2024-09-08 15:04:02 +0100

    added web server functionality to serve game in brower via flask

--- src/WebServer.py
+++ src/WebServer.py
@@ -1,11 +1,17 @@
 from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build/web')
 
+# Serve the index.html at the root URL
 @app.route('/')
-def home():
-    # Serve the image located in the 'static' folder
-    return send_from_directory('resources/', 'index.html')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+# Serve any other file in the build/web directory
+@app.route('/<path:filename>')
+def serve_static_files(filename):
+    return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
