c88347f5544ed83a635aa57825293c5766758f0a
MTelford <operator_141@outlook.com>
2024-09-10 20:52:45 +0100

    added uml class diagrams for all classes and sequence diagram for UI - Dealer interactions

--- src/EventHandler.py
+++ src/EventHandler.py
@@ -11,9 +11,3 @@ class EventHandler:
             if card is not None:
                 self.ui.draw_card(card)
 
-        # if event.type == pygame.MOUSEMOTION:
-        #     print("mouse moving")
-        #
-        # elif event.type == pygame.MOUSEBUTTONUP:
-        #     self.dragging = False
-        #     print("mouse release")
